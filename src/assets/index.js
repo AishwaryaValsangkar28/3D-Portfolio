bdc9487d4107b0ab2f3e2b5ff6df6cbc5bacd58e
AishwaryaValsangkar28 <valsangkaraishwarya987@gmail.com>
2026-02-16 22:10:27 +0530

    #1 added code for portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import dotnet from "./dotnet.png";
+import python from "./python.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import angular from "./tech/angular.png";
+import Dotnet from "./tech/Dotnet.png";
+import Github from "./tech/Github.png";
+import java from "./tech/java.png";
+import mysql from "./tech/mysql.png";
+import postgresql from "./tech/postgresql.png";
+import Python from "./tech/Python.png";
+
+import iam from "./company/iam.png";
+
+import voiceassistant from "./voiceassistant.png";
+import texttosql from "./texttosql.png";
+import bulkencode from "./bulkencode.png";
+import databasesync from "./databasesync.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  dotnet,
+  angular,
+  java,
+  mysql,
+  postgresql,
+  python,
+  Dotnet,
+  Python,
+  Github,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  iam,
+  voiceassistant,
+  texttosql,
+  bulkencode,
+  databasesync,
+};
